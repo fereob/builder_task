d8ae5057ab271650ee3ec476c93fa687bf076eff
Fernando <fortega@territoriosinteligentes.net>
2022-07-28 14:28:45 -0500

    Subida a git

---
+++ src/oscarblancarte/ipd/builder/dto/Department.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oscarblancarte.ipd.builder.dto;
+
+/**
+ *
+ * @author Fernando
+ */
+public class Department {
+    private String name;
+    
+    public Department(){
+        
+    }
+    
+    public Department(String name){
+        this.name = name;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    @Override
+    public String toString(){
+        return "Department{" + "name=" + name +"}";
+    }
+}
